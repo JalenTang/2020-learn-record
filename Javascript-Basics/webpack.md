74fd933ddc94092dd2f016f794170c9b4ffe0895
jalen <jalen2017@hotmail.com>
2020-05-21 20:12:45 +0800

    2020-5-21 贪心算法

---
+++ Javascript-Basics/webpack.md
@@ -0,0 +1,5 @@
+# webpack.js
+
+## 基本概念
+
+- webpack是一个现代的JavaScript应用的静态模块打包器（module bundler）,
--- test.js
+++ test.js
@@ -1,12 +1,35 @@
-function throttle(func, wait) {
-    let previous = 0
+/**
+ * @param {number[]} cost
+ * @param {number[]} val
+ * @param {number} total
+ * @return {number}
+ */
+const zeroOnePack = function (cost, val, total) {
+    const dp = []
 
-    return function() {
-        const context = this
-        const now = +Date().now()
-        if (now - previous > wait) {
-            func.apply(context, arguments)
-            previous = now
-        } 
+    const base = Array(total + 1).fill(0)
+    dp.push(base)
+
+    const len = val.length
+    for (let i = 1; i < len + 1; i++) {
+        dp.push([0])
+        for (let j = 1; j < total + 1; j++) {
+            let [inPack, notInPack] = [0, 0]
+            if (j >= cost[i - 1]) {
+                inPack = dp[i - 1][j - cost[i - 1]] + val[i - 1]  // 装
+                notInPack = dp[i - 1][j] // 不装
+                dp[i][j] = Math.max(inPack, notInPack)
+            } else {
+                dp[i][j] = dp[i - 1][j] // 只能不装
+            }
+        }
     }
-}
+
+    console.log(dp);
+
+    return dp[len][total]
+};
+
+const res = zeroOnePack([1, 2, 3], [2, 3, 1], 6);
+console.log(res);
+
