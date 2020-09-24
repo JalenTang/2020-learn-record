3bac78a967ca9e94e805239e430298d71d4c15a8
jalen <jalen2017@hotmail.com>
2020-09-24 20:03:26 +0800

    20200924 leetcode-91

---
+++ 算法LeetCode/91.num-decodings.js
@@ -0,0 +1,16 @@
+/*
+    TAG ===> 递归
+    题目：字母 A-Z 对应数字 1-26 给定一个只包含数字的非空字符串，请计算解码方法的总数
+
+    例如：
+        输入："12"   输出；2
+        输入："226"   输出；3
+*/
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const numDecodings = function (s) {
+    
+};
